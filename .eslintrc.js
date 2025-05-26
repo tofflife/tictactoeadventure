592ca5dac41cc1c37c26f7eb6fa28bf5e945e99f
tofflife <tofflife@inbox.ru>
2025-05-26 19:28:11 +0400

    added eslint and gitattributes

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:react-native/all'
+  ],
+  plugins: ['react', 'react-native'],
+  env: {
+    'react-native/react-native': true,
+  },
+}; 
--- App.js
+++ App.js
@@ -1,5 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native';
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+  },
+});
+
 export default function App() {
   return (
     <View style={styles.container}>
@@ -7,17 +21,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#fff',
-  },
-});
